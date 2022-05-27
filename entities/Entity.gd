a0e3d97ebc280d7598198f528d9ba2cb5e828f76
microslaw <milosz.grunwald@gmail.com>
2022-05-27 13:05:01 +0200

    made enemies killable

--- entities/Entity.gd
+++ entities/Entity.gd
@@ -8,3 +8,5 @@ func _physics_process(delta):
 	move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
 	if abs(velocity.x)>512:
 		velocity.x = 512*sign(velocity.x)
+	if is_on_floor():
+		velocity.y = 0
--- entities/enemy/Enemy.gd
+++ entities/enemy/Enemy.gd
@@ -8,3 +8,12 @@ func _physics_process(delta):
 	if is_on_wall():
 		velocity.x = -velocity.x
 	
+
+
+func _on_detectstomp_body_entered(body:PhysicsBody2D):
+	if body.global_position.y > get_node("detectstomp").global_position.y:
+		return
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
+		
+	
