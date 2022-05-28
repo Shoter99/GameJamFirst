ab9e2718e18d2cecdd8af67bb7b13b67452e30bd
Wikator <wiktor@szymulewicz.com>
2022-05-28 14:21:15 +0200

    Added firing

--- Player/Bullet.gd
+++ Player/Bullet.gd
@@ -1,17 +1,12 @@
 extends KinematicBody2D
 
 
-export var speed : int = 200
+export var speed : int = 250
 var velocity : Vector2 = Vector2()
 
 
-
-func _ready():
-	velocity = Vector2(speed, 0)
-
-
-
 func _physics_process(delta : float):
+	velocity = Vector2(speed, 0)
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		self.queue_free()
