a8421c8f5c687e553eff755a90c9282a66e14bc3
Vivi <shoter998@gmail.com>
2022-05-30 16:17:25 +0200

    Revert "Revert "Merge branch 'FixingEnemies'""
    This reverts commit cf81e78f7e6d1a45326148e44c6c3a9abdd7d6fb.

--- entities/Entity.gd
+++ entities/Entity.gd
@@ -3,12 +3,14 @@ extends KinematicBody2D
 class_name Entity
 export var gravity = 256
 export var velocity: = Vector2(0.0,0.0)
+export var walking = true
 func _physics_process(delta):
 	
-	move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+	if walking:
+		move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
 	if abs(velocity.x)>512:
 		velocity.x = 512*sign(velocity.x)
 	if is_on_floor():
-		velocity.y = 0
+		velocity.y = min(0,velocity.y)
 	else:
 		velocity.y +=gravity*delta
