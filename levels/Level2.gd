bfe03a779d9b057fa97bce67d7806be17795a116
Vivi <shoter998@gmail.com>
2022-05-30 13:19:40 +0200

    added spawning in different points

--- levels/Level2.gd
+++ levels/Level2.gd
@@ -1,8 +1,14 @@
 extends Node2D
 
-
-
+onready var spawnList = [
+	$SpawnPoints/Point1,
+	$SpawnPoints/Point2,
+	$SpawnPoints/Point3,
+	
+]
+var rng = RandomNumberGenerator.new()
 func _ready():
+	
 	for i in 3:
 		if Global.current_evolution[i] == 1:
 			match i:
@@ -15,7 +21,11 @@ func _ready():
 					
 		
 func createPlayer(player):
+	rng.randomize()
+	var randomNumber = rng.randi_range(0,2)
+	var playerPos = spawnList[randomNumber]
 	var p = player.instance()
-	p.global_position = $PlayerSpawn.global_position
+	p.global_position = playerPos.global_position
+	p.global_position.y += 175
 	$Player.add_child(p)
-	
+#	$Player.global_position = playerPos.global_position
