e4473e253a78f17b0a665adc654f3d50da6b0bde
microslaw <milosz.grunwald@gmail.com>
2022-05-30 14:11:30 +0200

    fixed ghosts

--- entities/enemy/ghost/GhostEnemy.gd
+++ entities/enemy/ghost/GhostEnemy.gd
@@ -5,22 +5,25 @@ onready var enemyHealthBar = $EnemyHealthBar;
 onready var player = get_tree().get_nodes_in_group("Player")[0]
 
 func _ready():
+	walking = false
 	self.setMaxHp(1)
 	self.gravity = 0
-	velocity.x = 0
-	velocity.y = 0
+	#velocity.x = 0
+	#velocity.y = 0
 
 func _physics_process(delta):
 	var xDist = player.get_child(0).global_position.x - self.global_position.x
-	var yDist = player.get_child(0).global_position.x - self.global_position.x
-	#print(self.global_position.y, "a", player.position.y) 
-	if abs(xDist)<200 and  abs(yDist)< 200 and velocity.x != 0:
-		velocity.y = yDist
-		velocity.x = xDist
-
+	var yDist = player.get_child(0).global_position.y - self.global_position.y
+	
+	#print(self.global_position.x, "a", player.get_child(0).global_position.x, "a", xDist) 
+	if abs(xDist)<200 and  abs(yDist)< 200:
+		velocity.y = yDist*2
+		velocity.x = xDist*2
+#		print(velocity.x, velocity.y)
+	move_and_slide(velocity)
 	
 
 func _on_playerKiller_body_entered(body):
-	if body.name == "Player":
+	if body.is_in_group("Player"):
 		Global.update_life(-ghostDmg)
-		player.get_child(0).knock_up
+		player.get_child(0).knock_up( player.get_child(0).global_position.x - self.global_position.x)
