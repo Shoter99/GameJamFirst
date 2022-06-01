cbfdc6e938f3a20e79ef7de8bda5f8f64f955b98
microslaw <milosz.grunwald@gmail.com>
2022-06-01 18:42:48 +0200

    added fully working peepers

--- entities/enemy/EnemyTurret/EnemyPeeper.gd
+++ entities/enemy/EnemyTurret/EnemyPeeper.gd
@@ -1,10 +1,9 @@
 extends Enemy
 export var firingSpeed = 2.0
-
-
 var tillNextShot = firingSpeed
 var bullet = preload("res://entities/enemy/EnemyTurret/EnemyBullet.tscn")
 onready var player = get_tree().get_nodes_in_group("Player")[0]
+export var diagonalBulletVelocity = 64
 
 func _ready():
 	self.setMaxHp(1)
@@ -17,11 +16,12 @@ func rotation_to_player():
 	var y = self.global_position.y - player.get_child(0).global_position.y
 	var deviation = atan(y/x)
 	#print(deviation)
-	return deviation * sign(x)
+	return deviation
 	
 func set_animation():
-	var d = rotation_to_player() * 60
-	if sign(player.get_child(0).global_position.x - self.global_position.x)<0:
+	var x = sign(player.get_child(0).global_position.x - self.global_position.x)
+	var d = rotation_to_player() * 60 * x
+	if x <0:
 		get_node("RotatingPeeper").set_flip_h(true)
 	else:
 		get_node("RotatingPeeper").set_flip_h(false)
@@ -40,7 +40,7 @@ func _process(delta):
 func _physics_process(delta):
 	tillNextShot -=delta
 	if tillNextShot <= 0:
-		#fire()
+		fire()
 		tillNextShot = firingSpeed
 
 func fire():
@@ -48,7 +48,16 @@ func fire():
 	var bulletFired = bullet.instance()
 	bulletFired.global_position.x = self.global_position.x
 	bulletFired.global_position.y = self.global_position.y-8
+	var x = player.get_child(0).global_position.x - self.global_position.x
+	var y = self.global_position.y - player.get_child(0).global_position.y
+	bulletFired.global_rotation = -atan2(y,x)
+	var vscale = diagonalBulletVelocity/sqrt(x*x+y*y)
+	#bulletFired.velocity = Vector2(x*vscale, y*vscale)
+	
 	get_tree().root.add_child(bulletFired)
+	bulletFired.set_velocity(Vector2(x*vscale, -y*vscale))
+	bulletFired.scale = Vector2(-1,-1)
+	
 	
 func _on_EnemyTurretArea2D_body_entered(body):
 	#print(body.name)
