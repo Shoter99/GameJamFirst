e28a4968bfd2c079da799eac32cb4100c36d137a
Vivi <shoter998@gmail.com>
2022-06-02 19:28:18 +0200

    added credits

--- UI/MainMenu.gd
+++ UI/MainMenu.gd
@@ -7,3 +7,15 @@ extends Control
 
 func _on_Start_pressed():
 	get_tree().change_scene("res://levels/Level2.tscn")
+
+
+func _on_Credits_pressed():
+	$CreditsText.visible = true
+
+
+func _on_Button_pressed():
+	$CreditsText.visible = false
+
+
+func _on_Exit_pressed():
+	get_tree().quit()
